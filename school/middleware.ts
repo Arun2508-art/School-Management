2c7665da795a1d991d9a1be277d8549b805e0d6e
Arun Kumar VEERAKUMAR <aruncode0125@gmail.com>
2025-05-07 13:32:41 +0800

    test without middleware

--- school/middleware.ts
+++ school/middleware.ts
@@ -78,6 +78,6 @@ export function middleware(request: NextRequest) {
   return NextResponse.next();
 }
 
-export const config = {
-  matcher: '/((?!_next/static|_next/image|favicon.ico|api/).*)'
-};
+// export const config = {
+//   matcher: '/((?!_next/static|_next/image|favicon.ico|api/).*)'
+// };
